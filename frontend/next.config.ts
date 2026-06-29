cb2c4921fc3c454dc42a8ac64e676f27140890bc
Priyanshu Paul <Priyanshupaul32@gmail.com>
2026-06-29 20:29:06 +0530

    updated the dev origins.

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,5 +1,9 @@
 import type { NextConfig } from "next";
 
+module.exports = {
+  allowedDevOrigins: ['10.108.152.60']
+}
+
 const nextConfig: NextConfig = {
   output: "standalone",
   /* config options here */
